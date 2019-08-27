efe3ee5540c15f73f7dfa264e557d6adffd32087
Rody Davis <rody.davis.jr@gmail.com>
2019-08-27 13:24:44 -0400

    Update creds.dart

--- example/lib/creds.dart
+++ example/lib/creds.dart
@@ -1,13 +1,6 @@
 import 'package:firebase_rest_api/api.dart';
 
 const firebaseApp = App(
-  // apiKey: 'FIREBASE_API_KEY',
-  // projectId: 'project-id',
-  apiKey: "AIzaSyCCAEs_QDl-bKQ5ZZx0MjdPk6XqsSsEsx0",
-  authDomain: "sandbox-79509.firebaseapp.com",
-  databaseURL: "https://sandbox-79509.firebaseio.com",
-  projectId: "sandbox-79509",
-  storageBucket: "",
-  messagingSenderId: "143797816149",
-  appId: "1:143797816149:web:5d85373aa91d436f",
+  apiKey: 'FIREBASE_API_KEY',
+  projectId: 'project-id',
 );
