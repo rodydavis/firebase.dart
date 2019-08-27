a48ce4f601c79e7f2513ca98ef2abb1ca5c600d9
Rody Davis <rody.davis.jr@gmail.com>
2019-08-27 15:19:30 -0400

    refactor

--- lib/src/impl/common/http.dart
+++ lib/src/impl/common/http.dart
@@ -18,138 +18,6 @@ abstract class FirestoreHttpClient implements FirestoreClient {
   @override
   FirestoreAccessToken token;
 
-  bool isCurrentTokenValid(bool refreshable) {
-    if (token == null) {
-      return false;
-    }
-
-    if (refreshable) {
-      var now = DateTime.now();
-      return token.expiresAt.difference(now).abs().inSeconds >= 60;
-    }
-    return true;
-  }
-
-  @override
-  bool get isAuthorized => isCurrentTokenValid(false);
-
-  @override
-  Future login(String email, String password) async {
-    var result = await getJsonMap(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${app.apiKey}',
-      body: {
-        "email": email,
-        "password": password,
-        "returnSecureToken": true,
-      },
-      extract: null,
-      needsToken: false,
-    );
-
-    token = FirestoreJsonAccessToken(result, DateTime.now());
-    return;
-  }
-
-  @override
-  Future loginAnonymously() async {
-    var result = await getJsonMap(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${app.apiKey}',
-      body: {
-        "returnSecureToken": true,
-      },
-      extract: null,
-      needsToken: false,
-    );
-
-    token = FirestoreJsonAccessToken(result, DateTime.now());
-    return;
-  }
-
-  @override
-  Future signUp(String email, String password) async {
-    var result = await getJsonMap(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${app.apiKey}',
-        body: {
-          "email": email,
-          "password": password,
-          "returnSecureToken": true,
-        },
-        needsToken: false);
-
-    token = FirestoreJsonAccessToken(result, DateTime.now());
-    return;
-  }
-
-  @override
-  Future<UserProviders> fetchProvidersForEmail(String email) async {
-    final _url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:createAuthUri?key=${app.apiKey}';
-    var result = await getJsonMap(
-      _url,
-      body: {
-        "identifier": email,
-        "continueUri": _url,
-      },
-      extract: null,
-      needsToken: false,
-    );
-
-    return UserProviders(this, result);
-  }
-
-  @override
-  Future<String> sendPasswordReset(String email) async {
-    final _url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=${app.apiKey}';
-
-    var result = await getJsonMap(
-      _url,
-      body: {
-        'requestType': 'PASSWORD_RESET',
-        "identifier": email,
-      },
-      extract: null,
-      needsToken: false,
-    );
-
-    return result['email'];
-  }
-
-  @override
-  Future<String> verifyPasswordResetCode(String code) async {
-    final _url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:resetPassword?key=${app.apiKey}';
-
-    var result = await getJsonMap(
-      _url,
-      body: {
-        "oobCode": code,
-      },
-      extract: null,
-      needsToken: false,
-    );
-
-    return result['email'];
-  }
-
-  @override
-  Future<String> confirmPasswordReset(String code, String password) async {
-    final _url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:resetPassword?key=${app.apiKey}';
-
-    var result = await getJsonMap(
-      _url,
-      body: {
-        "oobCode": code,
-        'newPassword': password,
-      },
-      extract: null,
-      needsToken: false,
-    );
-
-    return result['email'];
-  }
-
   @override
   Future changeEmailForUser(String email) async {
     var result = await getJsonMap(
@@ -185,6 +53,279 @@ abstract class FirestoreHttpClient implements FirestoreClient {
   }
 
   @override
+  CollectionReference collection(String path) {
+    assert(path != null);
+    return CollectionReference(this, path.split('/'));
+  }
+
+  @override
+  Future<String> confirmEmailVerification(String code) async {
+    var result = await getJsonMap(
+      'https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=${app.apiKey}',
+      body: {
+        "oobCode": code,
+      },
+      extract: null,
+      needsToken: false,
+    );
+    return result['email'];
+  }
+
+  @override
+  Future<String> confirmPasswordReset(String code, String password) async {
+    final _url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:resetPassword?key=${app.apiKey}';
+
+    var result = await getJsonMap(
+      _url,
+      body: {
+        "oobCode": code,
+        'newPassword': password,
+      },
+      extract: null,
+      needsToken: false,
+    );
+
+    return result['email'];
+  }
+
+  @override
+  Future deleteUserAccount(String id) async {
+    var result = await getJsonMap(
+      'https://identitytoolkit.googleapis.com/v1/accounts:delete?key=${app.apiKey}',
+      body: {
+        "idToken": id,
+      },
+      extract: null,
+      needsToken: false,
+    );
+
+    return;
+  }
+
+  @override
+  DocumentReference document(String path) {
+    assert(path != null);
+    return DocumentReference(this, path.split('/'));
+  }
+
+  @override
+  Future<UserProviders> fetchProvidersForEmail(String email) async {
+    final _url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:createAuthUri?key=${app.apiKey}';
+    var result = await getJsonMap(
+      _url,
+      body: {
+        "identifier": email,
+        "continueUri": _url,
+      },
+      extract: null,
+      needsToken: false,
+    );
+
+    return UserProviders(this, result);
+  }
+
+  @override
+  Future<FirebaseUser> getCurrentUser() async {
+    var result = await getJsonList(
+      'https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=${app.apiKey}',
+      body: {
+        "idToken": token.idToken,
+        "returnSecureToken": true,
+      },
+      extract: 'users',
+      needsToken: false,
+    );
+
+    if (result != null)
+      for (var item in result) {
+        final _user = FirebaseUser(this, item);
+        if (_user.uid == token.localId) {
+          return _user;
+        }
+      }
+    return null;
+  }
+
+  @override
+  Future<DocumentSnapshot> getDocumentSnapshot(String path) async {
+    final _data = await getJsonMap("$path", extract: null);
+    return DocumentSnapshot(this, _asStringKeyedMap(_data));
+  }
+
+  @override
+  Future<FirebaseUser> getUserInfo(String uid) async {
+    var result = await getJsonList(
+      'https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=${app.apiKey}',
+      body: {
+        "idToken": token.idToken,
+        "returnSecureToken": true,
+      },
+      extract: 'users',
+      needsToken: false,
+    );
+
+    if (result != null)
+      for (var item in result) {
+        final _user = FirebaseUser(this, item);
+        if (_user.uid == uid) {
+          return _user;
+        }
+      }
+    return null;
+  }
+
+  @override
+  Future<List<FirebaseUser>> getUsersForToken() async {
+    var list = <FirebaseUser>[];
+
+    var result = await getJsonList(
+      'https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=${app.apiKey}',
+      body: {
+        "idToken": token.idToken,
+        "returnSecureToken": true,
+      },
+      extract: 'users',
+      needsToken: false,
+    );
+
+    if (result != null)
+      for (var item in result) {
+        list.add(FirebaseUser(this, item));
+      }
+    return list;
+  }
+
+  @override
+  bool get isAuthorized => isCurrentTokenValid(false);
+
+  @override
+  Future linkWithEmailPasswordForUser(
+      String id, String email, String password) async {
+    var result = await getJsonMap(
+      'https://identitytoolkit.googleapis.com/v1/accounts:update?key=${app.apiKey}',
+      body: {
+        "idToken": id,
+        "password": password,
+        "returnSecureToken": true,
+      },
+      extract: null,
+      needsToken: false,
+    );
+
+    token = FirestoreJsonAccessToken(result, DateTime.now());
+    return;
+  }
+
+  @override
+  Future<List<DocumentSnapshot>> listDocumentSnapshots(String path) async {
+    var list = <DocumentSnapshot>[];
+
+    var result = await getJsonList("$path", extract: 'documents');
+
+    if (result != null)
+      for (var item in result) {
+        list.add(new DocumentSnapshot(this, item));
+      }
+
+    return list;
+  }
+
+  @override
+  Future login(String email, String password) async {
+    var result = await getJsonMap(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${app.apiKey}',
+      body: {
+        "email": email,
+        "password": password,
+        "returnSecureToken": true,
+      },
+      extract: null,
+      needsToken: false,
+    );
+
+    token = FirestoreJsonAccessToken(result, DateTime.now());
+    return;
+  }
+
+  @override
+  Future loginAnonymously() async {
+    var result = await getJsonMap(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${app.apiKey}',
+      body: {
+        "returnSecureToken": true,
+      },
+      extract: null,
+      needsToken: false,
+    );
+
+    token = FirestoreJsonAccessToken(result, DateTime.now());
+    return;
+  }
+
+  @override
+  Future<String> sendEmailVerificationForUser() async {
+    var result = await getJsonMap(
+      'https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=${app.apiKey}',
+      body: {
+        "idToken": token.idToken,
+        "requestType": 'VERIFY_EMAIL',
+      },
+      extract: null,
+      needsToken: false,
+    );
+    return result['email'];
+  }
+
+  @override
+  Future<String> sendPasswordReset(String email) async {
+    final _url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=${app.apiKey}';
+
+    var result = await getJsonMap(
+      _url,
+      body: {
+        'requestType': 'PASSWORD_RESET',
+        "identifier": email,
+      },
+      extract: null,
+      needsToken: false,
+    );
+
+    return result['email'];
+  }
+
+  @override
+  Future signUp(String email, String password) async {
+    var result = await getJsonMap(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${app.apiKey}',
+        body: {
+          "email": email,
+          "password": password,
+          "returnSecureToken": true,
+        },
+        needsToken: false);
+
+    token = FirestoreJsonAccessToken(result, DateTime.now());
+    return;
+  }
+
+  @override
+  Future unlinkProvidersForUser(List<String> providers) async {
+    var result = await getJsonMap(
+      'https://identitytoolkit.googleapis.com/v1/accounts:update?key=${app.apiKey}',
+      body: {
+        "idToken": token.idToken,
+        "deleteProvider": providers,
+      },
+      extract: null,
+      needsToken: false,
+    );
+    return;
+  }
+
+  @override
   Future updateProfileForUser({String displayName, String photoUrl}) async {
     var result = await getJsonMap(
       'https://identitytoolkit.googleapis.com/v1/accounts:update?key=${app.apiKey}',
@@ -211,173 +352,32 @@ abstract class FirestoreHttpClient implements FirestoreClient {
   }
 
   @override
-  Future<List<FirebaseUser>> getUsersForToken() async {
-    var list = <FirebaseUser>[];
+  Future<String> verifyPasswordResetCode(String code) async {
+    final _url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:resetPassword?key=${app.apiKey}';
 
-    var result = await getJsonList(
-      'https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=${app.apiKey}',
-      body: {
-        "idToken": token.idToken,
-        "returnSecureToken": true,
-      },
-      extract: 'users',
-      needsToken: false,
-    );
-
-    if (result != null)
-      for (var item in result) {
-        list.add(FirebaseUser(this, item));
-      }
-    return list;
-  }
-
-  @override
-  Future<FirebaseUser> getUserInfo(String uid) async {
-    var result = await getJsonList(
-      'https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=${app.apiKey}',
-      body: {
-        "idToken": token.idToken,
-        "returnSecureToken": true,
-      },
-      extract: 'users',
-      needsToken: false,
-    );
-
-    if (result != null)
-      for (var item in result) {
-        final _user = FirebaseUser(this, item);
-        if (_user.uid == uid) {
-          return _user;
-        }
-      }
-    return null;
-  }
-
-  @override
-  Future<FirebaseUser> getCurrentUser() async {
-    var result = await getJsonList(
-      'https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=${app.apiKey}',
-      body: {
-        "idToken": token.idToken,
-        "returnSecureToken": true,
-      },
-      extract: 'users',
-      needsToken: false,
-    );
-
-    if (result != null)
-      for (var item in result) {
-        final _user = FirebaseUser(this, item);
-        if (_user.uid == token.localId) {
-          return _user;
-        }
-      }
-    return null;
-  }
-
-  @override
-  Future linkWithEmailPasswordForUser(
-      String id, String email, String password) async {
     var result = await getJsonMap(
-      'https://identitytoolkit.googleapis.com/v1/accounts:update?key=${app.apiKey}',
-      body: {
-        "idToken": id,
-        "password": password,
-        "returnSecureToken": true,
-      },
-      extract: null,
-      needsToken: false,
-    );
-
-    token = FirestoreJsonAccessToken(result, DateTime.now());
-    return;
-  }
-
-  @override
-  Future unlinkProvidersForUser(List<String> providers) async {
-    var result = await getJsonMap(
-      'https://identitytoolkit.googleapis.com/v1/accounts:update?key=${app.apiKey}',
-      body: {
-        "idToken": token.idToken,
-        "deleteProvider": providers,
-      },
-      extract: null,
-      needsToken: false,
-    );
-    return;
-  }
-
-  @override
-  Future<String> sendEmailVerificationForUser() async {
-    var result = await getJsonMap(
-      'https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=${app.apiKey}',
-      body: {
-        "idToken": token.idToken,
-        "requestType": 'VERIFY_EMAIL',
-      },
-      extract: null,
-      needsToken: false,
-    );
-    return result['email'];
-  }
-
-  @override
-  Future<String> confirmEmailVerification(String code) async {
-    var result = await getJsonMap(
-      'https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=${app.apiKey}',
+      _url,
       body: {
         "oobCode": code,
       },
       extract: null,
       needsToken: false,
     );
+
     return result['email'];
   }
 
-  @override
-  Future deleteUserAccount(String id) async {
-    var result = await getJsonMap(
-      'https://identitytoolkit.googleapis.com/v1/accounts:delete?key=${app.apiKey}',
-      body: {
-        "idToken": id,
-      },
-      extract: null,
-      needsToken: false,
-    );
+  bool isCurrentTokenValid(bool refreshable) {
+    if (token == null) {
+      return false;
+    }
 
-    return;
-  }
-
-  @override
-  Future<List<DocumentSnapshot>> listDocumentSnapshots(String path) async {
-    var list = <DocumentSnapshot>[];
-
-    var result = await getJsonList("$path", extract: 'documents');
-
-    if (result != null)
-      for (var item in result) {
-        list.add(new DocumentSnapshot(this, item));
-      }
-
-    return list;
-  }
-
-  @override
-  CollectionReference collection(String path) {
-    assert(path != null);
-    return CollectionReference(this, path.split('/'));
-  }
-
-  @override
-  DocumentReference document(String path) {
-    assert(path != null);
-    return DocumentReference(this, path.split('/'));
-  }
-
-  @override
-  Future<DocumentSnapshot> getDocumentSnapshot(String path) async {
-    final _data = await getJsonMap("$path", extract: null);
-    return DocumentSnapshot(this, _asStringKeyedMap(_data));
+    if (refreshable) {
+      var now = DateTime.now();
+      return token.expiresAt.difference(now).abs().inSeconds >= 60;
+    }
+    return true;
   }
 
   Future<Map<String, dynamic>> getJsonMap(
